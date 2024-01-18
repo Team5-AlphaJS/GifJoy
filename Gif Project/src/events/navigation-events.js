7289b2357a431ad7fac1b47958d734e75a38192c
Martin Valov <maritn.valov98@gmail.com>
2024-01-18 18:51:27 +0200

    implementing nav events

---
+++ Gif Project/src/events/navigation-events.js
@@ -0,0 +1,11 @@
+export const loadPage = (page = '') => {
+    switch (page) {
+        case 'trending':
+
+        case 'search':
+
+        case 'upload':
+
+        default: return null;
+    }
+}
