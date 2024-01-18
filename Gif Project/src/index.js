4f2b490a92be9b59c6aee278bc797549f8d8eb93
Martin Valov <maritn.valov98@gmail.com>
2024-01-18 18:29:02 +0200

    adding event listener for anchor upload

--- Gif Project/src/index.js
+++ Gif Project/src/index.js
@@ -1,15 +1,16 @@
 import { q } from './events/helpers.js';
 
-    document.addEventListener('DOMContentLoaded', (ev) => {
+document.addEventListener('DOMContentLoaded', (ev) => {
     // trending event listener
-      q('a#trending').addEventListener('click', () => {
-      console.log('trending');
+    q('a#trending').addEventListener('click', () => {
+        console.log('trending');
     });
+    
     // search event listener
-    document.addEventListener('DOMContentLoaded'), (ev) => {
-      q('input#search').addEventListener('click', () =>
-      console.log('search'));
-    }
+    q('input#search').addEventListener('click', () => {
+        console.log('search');
+    });
+
     // upload event listener
     q('a#upload').addEventListener('click', (ev) => {
         console.log('Upload');
