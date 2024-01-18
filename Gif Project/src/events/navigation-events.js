1853f711f4e9dd55bec39d4519ba28a058db13f1
Alexander Velikov <alexander.velikov.a55@learn.telerikacademy.com>
2024-01-19 00:09:54 +0200

    Implemented Home Page + Random Gif Anchor

--- Gif Project/src/events/navigation-events.js
+++ Gif Project/src/events/navigation-events.js
@@ -1,14 +1,17 @@
 import { loadTrendingGifs, uploadGif } from "../requests/request-service.js";
+import { toHomeView } from "../views/home-view.js";
 import { toTrendingView } from "../views/trending-view.js";
-import { toUpladView } from "../views/uploadView.js";
+import { toUploadView } from "../views/uploadView.js";
 import { q } from "./helpers.js";
+import { renderRandomGif } from "./random-gif-events.js";
 
 export const loadPage = (page = '') => {
     switch (page) {
+        case 'home':
+            return renderHome();
+
         case 'trending':
             return renderTrendingGifs();
-
-        case 'search':
 
         case 'upload':
             return renderUploadView();
@@ -17,29 +20,36 @@ export const loadPage = (page = '') => {
     }
 }
 
+const renderHome = () => {
+    q('div#content-container').innerHTML = toHomeView();
+    q('a#random-gif').addEventListener('click', () => {
+        renderRandomGif();
+    });
+};
+
 const renderTrendingGifs = async () => {
     const trendingGifs = await loadTrendingGifs();
 
     q('div#content-container').innerHTML = toTrendingView(trendingGifs);
-}
+};
 
 const renderUploadView = () => {
-    q('div#content-container').innerHTML = toUpladView();
+    q('div#content-container').innerHTML = toUploadView();
     q('input[type="submit"]').addEventListener('click', async (ev) => {
         ev.preventDefault();
 
         const fileInput = q('input[name="gif-file"]');
-        
+
         const gifFile = fileInput.files[0];
 
         const formData = new FormData();
         formData.append('file', gifFile);
-        
+
         const options = {
             method: 'POST',
             body: formData
-        }
+        };
 
         await uploadGif(options);
     });
-}
+};
