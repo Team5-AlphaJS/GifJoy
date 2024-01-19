d70824bcbb02b8ee5389c8e43ec6aa3e5c1584fd
Alexander Velikov <alexander.velikov.a55@learn.telerikacademy.com>
2024-01-20 00:42:07 +0200

    some changes to the detailed view

--- Gif Project/src/views/gif-detailed-view.js
+++ Gif Project/src/views/gif-detailed-view.js
@@ -1,17 +1,15 @@
 export const toGifDetailedView = (gif) => `
-<div id="movies">
+<div id="gif-detailed">
   <h1>${gif.title} by ${gif.user.username}</h1>
   <div class="content">
-    <div class="movie-detailed">
-      <div class="poster">
+      <div class="gif-image">
         <img src="${gif.images.fixed_width.url}">
       </div>
-      <div class="movie-info">
+      <div class="gif-info">
         <a href="${gif.user.profile_url}">${gif.user.username}</a>
         <p>Title: Test</p>
         <button class="share-button" id="${gif.id}">Share:</button>
       </div>
-    </div>
   </div>
 </div>
 `;
