8d1dd5b63d1d9ac2e457373954033d93151b3349
Alexander Velikov <alexander.velikov.a55@learn.telerikacademy.com>
2024-01-22 00:14:56 +0200

    Some styling (temp logo, page layouts etc.)

--- Gif Project/src/views/gif-detailed-view.js
+++ Gif Project/src/views/gif-detailed-view.js
@@ -2,13 +2,13 @@ import { renderFavoriteStatus } from "../events/helpers.js";
 
 export const toGifDetailedView = (gif) => `
 <div id="gif-detailed">
-  <h1>${gif.title} by ${gif.user.username}</h1>
+  <h1>${gif.title}</h1>
   <div class="content">
       <div class="gif-image">
         <img src="${gif.images.fixed_width.url}">
       </div>
       <div class="gif-info">
-        <a href="${gif.user.profile_url}">${gif.user.username}</a>
+        <a href="${gif.user.profile_url}">User: ${gif.user.username}</a>
         <p>Title: Test</p>
         <button class="share-button" id="${gif.id}">Share:</button>
         ${renderFavoriteStatus(gif.id)}
--- Gif Project/src/views/home-view.js
+++ Gif Project/src/views/home-view.js
@@ -1,6 +1,6 @@
 export const toHomeView = () => `
 <div id="home">
-  <h1>Welcome to GifIt</h1>
+  <h1>Welcome to <img id="logo" src="src/assets/gifit-temporary-logo.png"></h1>
   <a href="#" id="random-gif">Click for a random gif :)</a>
   <div id="random-gif-container">
     
